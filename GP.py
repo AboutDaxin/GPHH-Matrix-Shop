e2201814e90f80865e445c39e80a67126bd40692
AboutDaxin <765974764@qq.com>
2023-08-01 14:49:30 +0800

    如何存储规则？

--- GP.py
+++ GP.py
@@ -11,11 +11,11 @@ import time
 # 设置一个常数K，用于后边锦标赛法选择子代
 K_CONST = 5
 # 最大个体评估次数（包含初代的个体）
-MAX_EVALUATIONS = 2700
+MAX_EVALUATIONS = 500
 # 最小步长(弃用)
 MIN_DELTA = 0.001
 # 运行多少次
-RUNS = 10
+RUNS = 1
 
 
 # 定义GP类
